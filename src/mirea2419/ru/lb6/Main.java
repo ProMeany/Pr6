ce29da42fe7d963c3811dd3c8577bdd55591265a
Рим <rimgainetdinov@yandex.ru>
2020-10-07 22:44:11 +0300

    Initial commit

---
+++ src/mirea2419/ru/lb6/Main.java
@@ -0,0 +1,49 @@
+package mirea2419.ru.lb6;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void quickSort(int[] array, int low, int high) {
+        if (array.length == 0)
+            return;
+        if (low >= high)
+            return;
+        int middle = low + (high - low) / 2;
+        int opora = array[middle];
+        int i = low, j = high;
+        while (i <= j) {
+            while (array[i] < opora) {
+                i++;
+            }
+
+            while (array[j] > opora) {
+                j--;
+            }
+
+            if (i <= j) {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        if (low < j)
+            quickSort(array, low, j);
+        if (high > i)
+            quickSort(array, i, high);
+    }
+    public static void main(String[] args) {
+        int[] x = { -35,5673,0,73,-234,532,-54};
+        System.out.println("Было");
+        System.out.println(Arrays.toString(x));
+
+        int low = 0;
+        int high = x.length - 1;
+
+        quickSort(x, low, high);
+        System.out.println("Стало");
+        System.out.println(Arrays.toString(x));
+    }
+}
